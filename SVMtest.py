24c2be86f9b7ff9dbc1cc70bf7920ace3038b9d8
lyb <2630784267@qq.com>
2023-11-15 17:41:39 +0800

    新增将矩阵压缩成一维的函数； 新增相对中心频率计算函数

--- SVMtest.py
+++ SVMtest.py
@@ -18,42 +18,62 @@ def main():
         folder = r'D:\IDM\下载\抑郁症数据集\MODMA数据集\EEG_3channels_resting_lanzhou_2015'
     elif (re.match('linux', sys.platform) != None):
         folder = '/home/lyb/data/MODMA_data/EEG_3channels_resting_lanzhou_2015'
-    files = []
+    # files = []
     labels = scipy.io.loadmat(os.path.join(folder, 'y_type.mat'))
-    labels = labels['y_type']
+    labels = labels['y_type'].squeeze()
 
     flag = True
     for file in os.listdir(folder):
         if fnmatch.fnmatch(file, '*_still.mat'):
-            files.append(file)
+            # files.append(file)
+            feature = np.array([])
             data = scipy.io.loadmat(os.path.join(folder, file))
             data = data['data'] / 1000000
             data = data.T
+            data = data[:, list(range(60 * 250))]
             newraw = myfe.filter_eeg(data, 250, 0.5, 100, 50)
+
             PSE = myfe.pse(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
-            PSE = PSE.reshape(1, PSE.shape[0] * PSE.shape[1])
+            PSE = myfe.compress(PSE)
+            PSE = PSE[[2, 4, 10, 11]]
+
+            CF = myfe.cf(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
+            CF = myfe.compress(CF)
+            CF = CF[[16]]
+
+            RCF = myfe.rcf(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
+            RCF = myfe.compress(RCF)
+            RCF = RCF[[8]]
+
+            feature = np.append(feature, PSE)
+            feature = np.append(feature, CF)
+            #feature = np.append(feature, RCF)
+
             if flag:
                 flag = False
-                features = PSE
+                features = feature
                 continue
-            features = np.vstack([features, PSE])
+            features = np.vstack((features, feature))
 
+    x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
+
+    # 利用网格搜索得出较好的超参数
     param_grid = {'C': [0.01, 1, 10, 100],
                   'gamma': [1, 0.1, 0.01, 0.001],
-                  # 'degree': [0, 1, 2, 3],
-                  'kernel': ['rbf']}
+                  'degree': [0, 1, 2, 3],
+                  'kernel': ['rbf', 'poly', 'linear']}
 
     model = svm.SVC()
     # 创建一个GridSearchCV对象
     grid = GridSearchCV(model, param_grid, refit=True, n_jobs=16, verbose=3)
     # 使用训练集的特征和标签训练分类器
-    grid.fit(features, labels.squeeze())
+    grid.fit(x_train, y_train)
     print("Best: %f using %s" % (grid.best_score_, grid.best_params_))
     y_pred = grid.predict(x_test)
 
-    # model = svm.SVC(C=100, gamma=0.001, kernel='rbf')
-    # x_train, x_test, y_train, y_test = train_test_split(features, labels, test_size=0.3)
-    # model.fit(x_train, y_train.squeeze())
+    # # SVM分类
+    # model = svm.SVC(C=100, gamma=1, kernel='rbf')
+    # model.fit(x_train, y_train)
     # # 使用测试集的特征进行预测
     # y_pred = model.predict(x_test)
 
--- myfe.py
+++ myfe.py
@@ -5,12 +5,12 @@ import matplotlib
 import matplotlib.pyplot as plt
 from scipy.stats import entropy
 
-band = [[0.5, 4],  # delta
-        [4, 8],  # theta
-        [8, 13],  # alpha
-        [13, 30],  # beta
-        [30, 100],  # gamma
-        [0.5, 100]]  # 全波段
+band = np.array([[0.5, 4],  # delta
+                 [4, 8],  # theta
+                 [8, 13],  # alpha
+                 [13, 30],  # beta
+                 [30, 100],  # gamma
+                 [0.5, 100]])  # 全波段
 
 
 # 对脑电数据进行高通、低通滤波和陷阱滤波的函数
@@ -22,17 +22,27 @@ def filter_eeg(eeg_matrix, sfreq, l_freq, h_freq, notch_freq):
     # 创建一个RawArray对象
     info = mne.create_info(ch_names=['channel' + str(i) for i in range(eeg_matrix.shape[0])],
                            sfreq=sfreq, ch_types='eeg')
-    raw = mne.io.RawArray(eeg_matrix, info, copy='both')  # 此处copy='both'可使传入的参数和
-    # RawArray对象中的参数独立修改，互不影响
+    raw = mne.io.RawArray(eeg_matrix, info, copy='both')  # 此处copy='both'可使传入的参数
+    # 和RawArray对象中的参数独立修改，互不影响
     # 应用高通和低通滤波器
-    # raw.filter(l_freq=l_freq, h_freq=h_freq)
+    raw.filter(l_freq=l_freq, h_freq=h_freq)
     # 应用陷阱滤波器
     raw.notch_filter(notch_freq, notch_widths=1)
     # 返回滤波后的数据
     return raw
 
 
-# 计算脑电信号的香农熵
+# 将多维特征矩阵压缩为一维np.array数组
+def compress(matrix):
+    if matrix.ndim == 1:
+        return matrix
+    m = matrix.shape[0]
+    n = matrix.shape[1]
+    new_matrix = matrix.reshape(1, m * n)[0]
+    return new_matrix
+
+
+# 计算脑电信号各通道的香农熵的函数
 def se(data, channels):
     # data为脑电信号矩阵，行为通道，列为采样点
     # 初始化各通道的香农熵
@@ -73,9 +83,9 @@ def pse(data, srate, channels):
         for j in range(0, 6):
             f_bool = (f >= band[j][0]) & (f <= band[j][1])
             f_band = f[f_bool]
-            pxx_band = pxx[i,f_bool]
+            pxx_band = pxx[i, f_bool]
             # pxx_prob = pxx_band / np.sum(pxx_band)    不需要，scipy.stats.entropy()会自动对数据进行归一化
-            PSE[i,j] = entropy(pxx_band,base=2)
+            PSE[i, j] = entropy(pxx_band, base=2)
 
             # PSE[i, 0] = -np.sum(pxx[i, round(0.5 * c):4 * c] *
             #                     np.log2(pxx[i, round(0.5 * c):4 * c]))
@@ -85,7 +95,8 @@ def pse(data, srate, channels):
             # PSE[i, 4] = -np.sum(pxx[i, 30 * c:100 * c] * np.log2(pxx[i, 30 * c:100 * c]))
             # PSE[i, 5] = -np.sum(pxx[i, round(0.5 * c):100 * c] *
             #                     np.log2(pxx[i, round(0.5 * c):100 * c]))
-    # 画出各通道功率谱图
+
+    # # 画出各通道功率谱图
     # plt.figure()
     # plt.plot(f[0:100 * c], psd[0, 0:100 * c])
     # plt.plot(f[0:100 * c], psd[1, 0:100 * c])
@@ -94,10 +105,11 @@ def pse(data, srate, channels):
     # plt.ylabel('PSD [V^2/Hz]')
     # plt.grid(True)
     # plt.show()
+
     return PSE
 
 
-# 计算
+# 计算各波段、各通道中心频率的函数
 def cf(data, srate, channels):
     # data为脑电信号矩阵，行为通道，列为采样点
     # srate代表采样频率
@@ -110,33 +122,52 @@ def cf(data, srate, channels):
         for j in range(0, 6):
             f_bool = (f >= band[j][0]) & (f < band[j][1])
             f_band = f[f_bool]
-            psd_band = psd[i,f_bool]
-            CF[i,j] = np.sum(psd_band * f_band) / np.sum(psd_band)
+            psd_band = psd[i, f_bool]
+            CF[i, j] = np.sum(psd_band * f_band) / np.sum(psd_band)
 
     return CF
+
+
+# 计算各波段、各通道相对中心频率的函数
+def rcf(data, srate, channels):
+    # data为脑电信号矩阵，行为通道，列为采样点
+    # srate代表采样频率
+    # channels代表通道数
+    RCF = np.zeros((channels, 6))
+    CF = cf(data, srate, channels)
+
+    for i in range(channels):
+        RCF[i] = CF[i] / CF[i][-1]
+
+    return RCF
 
 
 # 用于函数编写时的测试
 def main():
     mne.set_log_level(verbose='WARNING')
     data = scipy.io.loadmat(
-        r"D:\IDM\下载\抑郁症数据集\MODMA数据集\EEG_3channels_resting_lanzhou_2015\02010001_still.mat")
+        r"D:\IDM\下载\抑郁症数据集\MODMA数据集\EEG_3channels_resting_lanzhou_2015\02020018_still.mat")
     data = data['data'] / 1000000
     data = data.T
+    data = data[:, list(range(60 * 250))]
     newraw = filter_eeg(data, 250, 0.5, 100, 50)
 
     # 功率谱熵
-    # p = pse(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
-    # print(p)
-    # plt.show()
+    p = pse(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
+    print(p)
+    plt.show()
 
-    # 香农熵
+    # # 香农熵
     # s = se(newraw.get_data(), newraw.info['nchan'])
     # print(s)
 
-    #中心频率
-    # q = cf(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
-    # print(q)
+    # 中心频率
+    q = cf(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
+    print(q)
+
+    # 相对中心频率
+    r = rcf(newraw.get_data(), newraw.info['sfreq'], newraw.info['nchan'])
+    print(r)
 
 
 if __name__ == '__main__':
